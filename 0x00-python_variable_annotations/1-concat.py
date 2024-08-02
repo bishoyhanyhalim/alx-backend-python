0bb88483772d69f1aecdd4cf3eb7bf19a11da09c
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-01 17:12:07 -0700

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Write function concat takes a string
+"""Write function takes a string
 """
 
 
 def concat(str1: str, str2: str) -> str:
+    """func take two strings"""
     return str1 + str2
