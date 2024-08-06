afdfab49fed6b1984d5de3ca8e5a5f351ef78424
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-05 17:37:41 -0700

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""this is a function for task 0"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait time"""
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
